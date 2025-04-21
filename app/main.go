26f276602aa4362c5cfb6a8dcf10f1f6c31359d7
Chan Ming Han <chanminghan00@gmail.com>
2025-04-21 22:50:04 +0800

    parse correlation ID

--- app/main.go
+++ app/main.go
@@ -15,7 +15,7 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
@@ -27,7 +27,27 @@ func main() {
 		os.Exit(1)
 	}
 
+
+	// Example request message
+	//	00 00 00 23  // message_size:        35 			-> 4 byte
+	//	00 12        // request_api_key:     18				-> 2 byte
+	// 	00 04        // request_api_version: 4				-> 2 byte
+	// 	6f 7f c6 61  // correlation_id:      1870644833		-> 4 byte
+
+	buf := make([]byte, 1024)
+
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("read err:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("request: %s\n", buf)
+
+	corrID := buf[8:12]
+
+	fmt.Printf("corrId: %x\n", corrID)
+
 	// messageSize(4byte) | correlationID(4byte) | Body...
-	defer conn.Close()
-	conn.Write([]byte{1,1,1,1,0,0,0,7}) 
+	defer conn.Close() // we need to close the connection after function exit
+	conn.Write([]byte{1, 1, 1, 1, buf[8], buf[9], buf[10], buf[11]})
 }
