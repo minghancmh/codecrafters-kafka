f6e07d57b900cb6f3afdea4ce717d35ae651a713
Chan Ming Han <chanminghan00@gmail.com>
2025-04-21 23:16:06 +0800

    invalid api version

--- app/main.go
+++ app/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"encoding/binary"
+	"bytes"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+var UNSUPPORTED_VERSION uint16 = 35
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -28,13 +32,14 @@ func main() {
 	}
 
 
-	// Example request message
+	// Example request message (all in big endian)
 	//	00 00 00 23  // message_size:        35 			-> 4 byte
 	//	00 12        // request_api_key:     18				-> 2 byte
 	// 	00 04        // request_api_version: 4				-> 2 byte
 	// 	6f 7f c6 61  // correlation_id:      1870644833		-> 4 byte
 
 	buf := make([]byte, 1024)
+
 
 	_, err = conn.Read(buf)
 	if err != nil {
@@ -43,11 +48,66 @@ func main() {
 	}
 	fmt.Printf("request: %s\n", buf)
 
-	corrID := buf[8:12]
+	// parse the message
+	// message_size :=  binary.BigEndian.Uint32(buf[0:4])	
+	// requestAPIKey := binary.BigEndian.Uint16(buf[4:6])
+	requestAPIVersion :=  binary.BigEndian.Uint16(buf[6:8])
+	corrID := binary.BigEndian.Uint32(buf[8:12])
 
-	fmt.Printf("corrId: %x\n", corrID)
+	response := make([]byte, 1024)
+
+	setMessageSize(&response, 3735928559) // DEADBEEF for placeholder
+	setCorrelationId(&response, corrID)
+
+	fmt.Printf("response: %x\n", response)
+
+
+
+
+	if requestAPIVersion > 4 {
+		fmt.Printf("Requested API Version not supported: %d\n", requestAPIVersion)
+		setErrorCode(&response, UNSUPPORTED_VERSION)
+		conn.Write(response)
+		os.Exit(1)
+	}
+
 
 	// messageSize(4byte) | correlationID(4byte) | Body...
 	defer conn.Close() // we need to close the connection after function exit
-	conn.Write([]byte{1, 1, 1, 1, buf[8], buf[9], buf[10], buf[11]})
+	conn.Write(response)
 }
+
+func setMessageSize(buf *[]byte, msgSize uint32) {
+	tmp := new(bytes.Buffer)
+	err := binary.Write(tmp, binary.BigEndian, msgSize)
+	if err != nil {
+		fmt.Println("Setting msg size failed: ", err)
+		return
+	}
+	tmpBytes := tmp.Bytes()
+	copy((*buf)[0:4], tmpBytes)
+	return
+}
+
+func setCorrelationId(buf *[]byte, corrId uint32) {
+	tmp := new(bytes.Buffer)
+	err := binary.Write(tmp, binary.BigEndian, corrId)
+	if err != nil {
+		fmt.Println("Setting correlation ID failed: ", err)
+		return
+	}
+	tmpBytes := tmp.Bytes()
+	copy((*buf)[4:8], tmpBytes)
+}
+
+func setErrorCode(buf *[]byte, errorCode uint16) {
+	tmp := new(bytes.Buffer)
+	err := binary.Write(tmp, binary.BigEndian, errorCode)
+	if err != nil {
+		fmt.Println("Setting error code failed: ", err)
+		return
+	}
+	tmpBytes := tmp.Bytes()
+	copy((*buf)[8:10], tmpBytes)
+}
+
