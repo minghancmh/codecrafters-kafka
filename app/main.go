4ec87adad351f0f4a13a253df6d27c80818631ab
Chan Ming Han <chanminghan00@gmail.com>
2025-04-27 20:00:13 +0800

    print logs

--- app/main.go
+++ app/main.go
@@ -251,6 +251,7 @@ func getTopicByName(name string) TopicRecord {
 	if err != nil {
 		fmt.Println("Error reading file at path: ", path)
 	}
+	fmt.Printf("dat: %x", dat)
 	rb := readRecordBatch(dat, 0)
 
 	for _, rec := range rb.records {
